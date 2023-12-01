e5365614a5c78666db550d1028f7d5c5ae30c89f
SejinPrk <qkrwns9808@naver.com>
2023-12-01 15:55:26 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -4,19 +4,8 @@ import java.util.Scanner;
 
 public class App {
 
-  static final String ANSI_CLEAR = "\033[0m";
-  static final String ANSI_BOLD_RED = "\033[1;31m";
-  static final String ANSI_RED = "\033[0;31m";
-  static final String APP_TITLE = ANSI_BOLD_RED + "[과제관리 시스템]" + ANSI_CLEAR;
-  static final String[] MENUS = {
-      "1. 과제",
-      "2. 게시글",
-      "3. 도움말",
-      ANSI_RED + "4. 종료" + ANSI_CLEAR
-  };
-
   public static void main(String[] args) {
-    printMainMenu();
+    MainMenu.printMenu();
     java.util.Scanner keyIn = new java.util.Scanner(System.in);
 
     loop:
@@ -36,9 +25,7 @@ public class App {
           System.out.println("종료합니다.");
           break loop;
         case "menu":
-          // 코드를 기능 단위로 묶어 메서드로 정의하면
-          // 메서드의 이름을 통해 해당 기능을 쉽게 유추할 수 있어 유지보수에 좋다.
-          printMainMenu();
+          MainMenu.printMenu();
           break;
         default:
           System.out.println("메뉴 번호가 옳지 않습니다.");
@@ -47,14 +34,6 @@ public class App {
     keyIn.close();
   }
 
-  static void printMainMenu() {
-    System.out.println(APP_TITLE);
-    System.out.println();
-    for (String menu : MENUS) {
-      System.out.println(menu);
-
-    }
-  }
 
   static String prompt(String title, Scanner keyIn) {
     System.out.printf("%s> ", title);
