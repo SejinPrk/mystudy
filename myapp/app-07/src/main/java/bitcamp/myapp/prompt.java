1ae2e328df05d661251ef40aa89848143a5789a5
SejinPrk <qkrwns9808@naver.com>
2023-12-03 16:05:04 +0900

    ok

---
+++ myapp/app-07/src/main/java/bitcamp/myapp/prompt.java
@@ -0,0 +1,15 @@
+package bitcamp.myapp;
+
+import java.util.Scanner;
+Public class Prompt {
+
+  static Scanner keyIn = new Scanner(System.in);
+  static String input(String title) {
+    System.out.printf("%s> ", title);
+    return keyIn.nextLine();
+  }
+}
+
+ static void close() {
+  keyIn.close();
+ }
