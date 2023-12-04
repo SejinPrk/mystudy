1917a697d504fb2e0bea22b2ead14a22f524de5a
SejinPrk <qkrwns9808@naver.com>
2023-12-04 14:57:23 +0900

    ok

--- java-basic/app/src/main/java/com/eomcs/oop/ex02/step07/App.java
+++ java-basic/app/src/main/java/com/eomcs/oop/ex02/step07/App.java
@@ -19,25 +19,31 @@ public class App {
     // 식1) 2 + 3 - 1 * 7 / 3 = ?
     // 식2) 3 * 2 + 7 / 4 - 5 = ?
 
+    // com.eomcs.oop.ex02.step07.util.Calculator c1 = new com.eomcs.oop.ex02.step07.util.Calculator();
+    // com.eomcs.oop.ex02.step07.util.Calculator c2 = new com.eomcs.oop.ex02.step07.util.Calculator();
+    // 코드가 너무 길어지는 문제점 발생
+    // import 명령을 사용하여 클래스의 패키지 이름을 미리 알려준다면,
+    // 클래스를 지정할 때 패키지 이름을 나열할 필요가 없다.
+
     Calculator c1 = new Calculator();
     Calculator c2 = new Calculator();
 
     // 계산을 수행할 때 계산 결과를 보관할 메모리를 메서드 호출 앞에서 전달하라!
     // - 인스턴스 메서드를 사용하면 파라미터로 인스턴스 주소를 전달할 필요가 없다.
-    c1.plus(2); 
-    c2.plus(3); 
+    c1.plus(2);
+    c2.plus(3);
 
-    c1.plus(3); 
+    c1.plus(3);
     c2.multiple(2);
 
-    c1.minus(1); 
-    c2.plus(7); 
+    c1.minus(1);
+    c2.plus(7);
 
-    c1.multiple(7); 
+    c1.multiple(7);
     c2.divide(4);
 
-    c1.divide(3); 
-    c2.minus(5); 
+    c1.divide(3);
+    c2.minus(5);
 
     // 식1의 계산 결과는 c1 인스턴스의 result 변수에 들어 있고,
     // 식2의 계산 결과는 c2 인스턴스의 result 변수에 들어 있다.
