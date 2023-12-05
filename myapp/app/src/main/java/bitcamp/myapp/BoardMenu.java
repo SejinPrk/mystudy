415fb804e27e3721e4b04f43d796312f9266da33
SejinPrk <qkrwns9808@naver.com>
2023-12-05 17:45:26 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/BoardMenu.java
+++ myapp/app/src/main/java/bitcamp/myapp/BoardMenu.java
@@ -2,6 +2,11 @@ package bitcamp.myapp;
 
 public class BoardMenu {
 
+  static String title;
+  static String content;
+  static String writer;
+  static String createdDate;
+
   static void printMenu() {
     System.out.println("[게시글]");
     System.out.println("1. 등록");
@@ -25,16 +30,16 @@ public class BoardMenu {
 
       switch (input) {
         case "1":
-          System.out.println("등록입니다.");
+          add();
           break;
         case "2":
-          System.out.println("조회입니다.");
+          view();
           break;
         case "3":
-          System.out.println("변경입니다.");
+          modify();
           break;
         case "4":
-          System.out.println("삭제입니다.");
+          delete();
           break;
         case "0":
           return;
@@ -47,4 +52,35 @@ public class BoardMenu {
     }
   }
 
+  static void add() {
+    System.out.println("게시글 등록: ");
+    title = Prompt.input("제목? ");
+    content = Prompt.input("내용? ");
+    writer = Prompt.input("작성자? ");
+    createdDate = Prompt.input("작성일? ");
+  }
+
+  static void view() {
+    System.out.println("게시글 조회");
+    System.out.printf("제목: %s\n", title);
+    System.out.printf("내용: %s\n", content);
+    System.out.printf("작성자: %s\n", writer);
+    System.out.printf("작성일: %s\n", createdDate);
+  }
+
+  static void modify() {
+    System.out.println("게시글 변경: ");
+    title = Prompt.input("제목?(%s) ", title);
+    content = Prompt.input("내용?(%s) ", content);
+    writer = Prompt.input("작성자?(%s) ", writer);
+    createdDate = Prompt.input("작성일?(%s) ", createdDate);
+  }
+
+  static void delete() {
+    System.out.println("게시글 삭제: ");
+    title = "";
+    content = "";
+    writer = "";
+    createdDate = "";
+  }
 }
