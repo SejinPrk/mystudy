951d2e3eca43706bbcf0782111eec02405f17567
SejinPrk <qkrwns9808@naver.com>
2023-12-09 19:40:57 +0900

    .

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level01/exam07/Main.java
@@ -0,0 +1,11 @@
+package algorithm.test.baekjoon.level01.exam07;
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String ID = sc.nextLine();
+
+    System.out.printf("%s"+"??!", ID);
+
+  }
+}
