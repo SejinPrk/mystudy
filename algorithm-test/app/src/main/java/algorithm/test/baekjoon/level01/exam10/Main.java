ce71e0f97095f5ff0063d154157a86a481eabc3a
SejinPrk <qkrwns9808@naver.com>
2023-12-10 20:16:55 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level01/exam10/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level01/exam10/Main.java
@@ -1,2 +1,17 @@
-package algorithm.test.baekjoon.level01.exam10;public class Main {
-}
+package algorithm.test.baekjoon.level01.exam10;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main (String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int A = sc.nextInt();
+      int B = sc.nextInt();
+      sc.close();
+      System.out.println(A*(B%10));
+      System.out.println(A*(B%100/10));
+      System.out.println(A*(B/100));
+      System.out.println(A*B);
+    }
+  }
+
