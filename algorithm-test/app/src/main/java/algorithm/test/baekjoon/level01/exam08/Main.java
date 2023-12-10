d72323285418afe216ca5c9bbadd4296770fe3a4
SejinPrk <qkrwns9808@naver.com>
2023-12-10 20:01:12 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level01/exam08/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level01/exam08/Main.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    //int BuddhistYear = sc.nextInt();
-    int ADyear = sc.nextInt();
+    int BuddhistYear = sc.nextInt();
+    //int ADyear = sc.nextInt();
     sc.close();
-    //int ADyear= BuddhistYear - 543;
-    int BuddhistYear = ADyear - 543;
-    //System.out.println(ADyear);
-    System.out.println(BuddhistYear);
+    int ADyear= BuddhistYear - 543;
+    //int BuddhistYear = ADyear - 543;
+    System.out.println(ADyear);
+    //System.out.println(BuddhistYear);
   }
 }
