d77fd5dae2c1db620df8f11ab76d6edc5aff9cfb
SejinPrk <qkrwns9808@naver.com>
2023-12-11 11:50:55 +0900

    /

---
+++ myapp/app/src/main/java/bitcamp/myapp/menu/Menu.java
@@ -0,0 +1,10 @@
+package bitcamp.myapp.menu;
+
+// Menu를 처리하는 객체의 사용법을 정의한다.
+public interface Menu {
+
+  // 객체를 실행할 때 호출할 메서드를 선언한다.
+  // 구현을 해서는 안된다. 중괄호 {} 안됨.
+  // => 추상 메서드 (abstract)
+  public abstract void execute(); // public abstract는 기본 문법. 생략 가능!
+}
