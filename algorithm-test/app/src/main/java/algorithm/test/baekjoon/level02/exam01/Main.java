0637d183d33e432b982cd240a9d9d8ee989e95f2
SejinPrk <qkrwns9808@naver.com>
2023-12-12 11:43:04 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level02/exam01/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level02/exam01/Main.java
@@ -1,5 +1,19 @@
 package algorithm.test.baekjoon.level02.exam01;
+//1330 두 수 비교하
+import java.util.Scanner;
 
 public class Main {
 
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int A = sc.nextInt();
+    int B = sc.nextInt();
+    sc.close();
+    if (A>B)
+      System.out.println(">");
+    if(A<B)
+      System.out.println("<");
+    if (A==B)
+      System.out.println("==");
+  }
 }
---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level02/exam03/Main.java
@@ -0,0 +1,12 @@
+package algorithm.test.baekjoon.level02.exam03;
+
+import java.util.Scanner;
+
+  public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int year = sc.nextInt();
+      sc.close();
+     if (year%4 == 0 && )
+    }
+  }
