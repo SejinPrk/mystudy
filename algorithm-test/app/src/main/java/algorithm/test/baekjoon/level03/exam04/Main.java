f88cc1a8706c0d74a6b322453978b869827e9ff6
SejinPrk <qkrwns9808@naver.com>
2023-12-15 09:25:58 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/exam04/Main.java
@@ -1,7 +1,24 @@
 package algorithm.test.baekjoon.level03.exam04;
+// 25304 영수증
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args){
 
+    Scanner sc = new Scanner(System.in);
+    int X = sc.nextInt();
+    int N = sc.nextInt();
+    int sum = 0;
+
+    for(int i=0; i<N; i++){
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      sum += a*b;
+    }
+    if(sum==X){
+      System.out.println("Yes");
+    }else{
+      System.out.println("No");
+    }
   }
 }
