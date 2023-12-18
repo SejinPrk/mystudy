0a778cb08c3bff548bed49c9741ec7699606153d
SejinPrk <qkrwns9808@naver.com>
2023-12-18 09:34:26 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/exam06/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/exam06/Main.java
@@ -1,7 +1,25 @@
 package algorithm.test.baekjoon.level03.exam06;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 // 빠른 A+B
 public class Main {
-  public static void main(String[] args){
+  public static void main(String[] args) throws IOException {
 
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine(), " ");
+      sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append('\n');
+    }
+    br.close();
+
+   System.out.println(sb);
   }
 }
