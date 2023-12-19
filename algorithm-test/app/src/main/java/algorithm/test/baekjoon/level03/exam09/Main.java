05d27b39d48cf7bce15136d6b3835703276b3fbe
SejinPrk <qkrwns9808@naver.com>
2023-12-19 10:07:05 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/exam09/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/exam09/Main.java
@@ -1,7 +1,19 @@
 package algorithm.test.baekjoon.level03.exam09;
 
+import java.util.Scanner;
+
+// 2438 별 찍기-1
 public class Main {
   public static void main(String[] args){
 
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+
+    for(int i=1; i<=N; i++){
+      for(int j = 1; j <= i; j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 }
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/exam10/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level03/exam10/Main.java
@@ -1,7 +1,23 @@
 package algorithm.test.baekjoon.level03.exam10;
 
+import java.util.Scanner;
+
+// 2439 별 찍기 - 2
 public class Main {
   public static void main(String[] args){
 
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    sc.close();
+
+    for(int i=1; i<=N; i++){
+      for(int j=1; j<=N-i; j++){
+        System.out.print(" ");// 왼쪽부터 줄어드는 공백
+      }
+      for (int j=0; j<i; j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 }
