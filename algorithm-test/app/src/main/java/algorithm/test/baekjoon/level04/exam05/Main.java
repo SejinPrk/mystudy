10e04e4dedf05cb39e453c66b8df7b44312ebb46
SejinPrk <qkrwns9808@naver.com>
2023-12-22 11:22:05 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam05/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam05/Main.java
@@ -1,5 +1,28 @@
 package algorithm.test.baekjoon.level04.exam05;
+
+import java.util.Scanner;
+
 // 10810 공 넣기
 public class Main {
+public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+  int N = sc.nextInt();
+  int M = sc.nextInt();
+  int arr[] = new int[N];
 
+  for (int i=0;i < M; i++){
+
+    int I = sc.nextInt();
+    int J = sc.nextInt();
+    int K = sc.nextInt();
+
+    for(int j = I - 1; j < J; j++) {
+      arr[j] = K;
+    }
+  }
+  for(int k = 0; k < arr.length; k++) {
+    System.out.print(arr[k] + " ");
+    }
+  }
 }
+
