8f345aba9536dfc134ef80cee4bf2b0c6f5773ba
SejinPrk <qkrwns9808@naver.com>
2023-12-24 15:39:27 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam07/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam07/Main.java
@@ -1,5 +1,21 @@
 package algorithm.test.baekjoon.level04.exam07;
 
-public class Main {
+import java.util.Scanner;
 
+// 5597 과제 안내신 분..?
+public class Main{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int [] student = new int[30];
+
+    for(int i=0; i<28; i++){
+      int submitted = sc.nextInt();
+      student[submitted-1] = 1;
+    }
+    for (int i=0; i<student.length; i++){
+      if(student[i]== 0)
+        System.out.println(i+1);
+    }
+    sc.close();
+  }
 }
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam08/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level04/exam08/Main.java
@@ -1,5 +1,8 @@
 package algorithm.test.baekjoon.level04.exam08;
-
+// 3052 나머지
 public class Main {
-
+ public static void main(String[] args){
+   int A;
+   int B;
+ }
 }
