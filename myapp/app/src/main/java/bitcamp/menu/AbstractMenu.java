07dfd0be609560d5a8b105bad1f8ba9757f0cb37
SejinPrk <qkrwns9808@naver.com>
2023-12-28 15:26:46 +0900

    .

--- myapp/app/src/main/java/bitcamp/menu/AbstractMenu.java
+++ myapp/app/src/main/java/bitcamp/menu/AbstractMenu.java
@@ -1,19 +1,22 @@
 package bitcamp.menu;
 
+import bitcamp.util.Stack;
+
 public abstract class AbstractMenu implements Menu {
 
-  private String title;
-
-  public AbstractMenu(String title) {
+  protected Stack<String> breadcrumb;
+  String title;
+  public AbstractMenu(String title, Stack<String> breadcrumb) {
     this.title = title;
+    this.breadcrumb = breadcrumb;
   }
 
   @Override
   public String getTitle() {
-    return title;
+    return breadcrumb.peek();
   }
 
-  public void setTitle(String title) {
-    this.title = title;
+  public String getMenuPath(){
+    return String.join("/", breadcrumb.toArray(new String[0])); // 배열의 값 중간에 슬래시를 하나씩 넣어준다.
   }
 }
---
+++ myapp/app/src/main/java/bitcamp/util/Queue.java
@@ -0,0 +1,17 @@
+package bitcamp.util;
+
+public class Queue<E> extends LinkedList<E> {
+  public boolean offer(E value){
+    this.add(value);
+    return true;
+  }
+  public E poll(){
+    if(this.size == 0) {
+      return null;
+    }
+    return this.remove(0);
+  }
+  public E peek(){
+    return this.get(this.size()-1);
+  }
+}
