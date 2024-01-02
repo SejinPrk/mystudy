95da859725128a61588649df4ad4dfaf501379c0
SejinPrk <qkrwns9808@naver.com>
2024-01-02 10:07:24 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/exam11/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level05/exam11/Main.java
@@ -1,11 +1,16 @@
 package algorithm.test.baekjoon.level05.exam11;
 // 11718 그대로 출력하기
-import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String s;
+    while ((s= br.readLine()) != null){
+      System.out.println(s);
+    }
   }
-
 }
