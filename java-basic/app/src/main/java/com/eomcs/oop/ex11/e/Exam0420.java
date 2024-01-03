e54ecd46ba437b110cdb3a6096be43a83391d4a1
SejinPrk <qkrwns9808@naver.com>
2024-01-03 10:26:14 +0900

    .

--- java-basic/app/src/main/java/com/eomcs/oop/ex11/e/Exam0420.java
+++ java-basic/app/src/main/java/com/eomcs/oop/ex11/e/Exam0420.java
@@ -14,4 +14,21 @@ public class Exam0420 {
       System.out.println("Hello!");
     }
   };
+  //컴파일 할 때 위의 문장은 다음의 문장으로 변환한다.
+  // class Exam0410$1 extends A{
+
+  //  @Override
+  //  public void print() {
+  //    System.out.println("Hello!");
+  //  }
+  //}
+  //
+  // A obj;
+  // int v1 = 100;
+  //
+  //static {
+  //  obj = new Exam0410%1(this);
+  //  v1 = 100;
+  //}
+
 }
