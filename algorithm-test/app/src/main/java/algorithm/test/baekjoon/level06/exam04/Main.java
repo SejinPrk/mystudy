d9a442c03c38b43bda1fa5647e2b7011a0afa3b2
SejinPrk <qkrwns9808@naver.com>
2024-01-04 09:46:08 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam04/Main.java
@@ -1,12 +1,21 @@
 package algorithm.test.baekjoon.level06.exam04;
 
 import java.util.Scanner;
+
 // 10988 팰린드롬인지 확인하기
 public class Main {
-
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    sc.close();
+    int n = str.length();
+    int a = 1; //true
 
+    for(int i = 0; i<n/2; i++){
+      if(str.charAt(i)!= str.charAt(n-1-i)){
+        a = 0; // false
+      }
+    }
+    System.out.println(a);
   }
-
 }
