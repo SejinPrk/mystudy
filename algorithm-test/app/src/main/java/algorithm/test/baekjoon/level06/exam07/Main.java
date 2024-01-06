cbcc8da16bcff334f9dbcdca7e5850b154f35915
SejinPrk <qkrwns9808@naver.com>
2024-01-06 17:00:57 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam07/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level06/exam07/Main.java
@@ -1,12 +1,44 @@
 package algorithm.test.baekjoon.level06.exam07;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
-import java.util.Scanner;
-
-// 1316 그룹 단어 체커
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
 
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+  public static void main(String[] args) throws IOException {
+
+    int count = 0;
+    int N = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < N; i++) {
+      if (check()) {
+        count++;
+      }
+    }
+    System.out.print(count);
   }
 
+  public static boolean check() throws IOException {
+    boolean[] check = new boolean[26];
+    int prev = 0;
+    String str = br.readLine();
+
+    for(int i = 0; i < str.length(); i++) {
+      int now = str.charAt(i);
+
+
+      if (prev != now) {
+        if (!check[now - 'a']) {
+          check[now - 'a'] = true;
+          prev = now;
+        }
+        else {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 }
