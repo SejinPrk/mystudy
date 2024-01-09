dd9f05c4105425fad98be5f0d02c83763f15ab21
SejinPrk <qkrwns9808@naver.com>
2024-01-09 10:32:01 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level07/exam04/Main.java
@@ -1,5 +1,32 @@
 package algorithm.test.baekjoon.level07.exam04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 // 2563 색종이
 public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int black = 0;
+    int n = Integer.parseInt(br.readLine());
+    boolean[][] arr = new boolean[100][100];
 
+    for(int i =0; i<n; i++){
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+
+      for (int j=x; j < x+10; j++){
+        for(int k=y; k < y+10; k++){
+          if(!arr[j][k]){
+            arr[j][k] = true;
+            black++;
+          }
+        }
+      }
+    }
+    System.out.println(black);
+  }
 }
