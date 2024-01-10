8aa560fe0b32130d22c24b520d3ccef2c53ca2f9
SejinPrk <qkrwns9808@naver.com>
2024-01-10 15:15:06 +0900

    .

--- java-basic/app/src/main/java/com/eomcs/annotation/ex1/Exam0120.java
+++ java-basic/app/src/main/java/com/eomcs/annotation/ex1/Exam0120.java
@@ -4,9 +4,9 @@ package com.eomcs.annotation.ex1;
 @MyAnnotation2(value="okok")
 public class Exam0120 {
 
-  @MyAnnotation2(value="okok")
+  @MyAnnotation2(value="okok2")
   int a;
 
-  @MyAnnotation2(value="okok")
+  @MyAnnotation2(value="okok3")
   void m() {}
 }
