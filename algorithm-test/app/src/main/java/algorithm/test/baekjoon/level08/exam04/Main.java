6403bdafca5ca8aca1666d43df92271860a3f9c5
SejinPrk <qkrwns9808@naver.com>
2024-01-13 16:49:05 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam04/Main.java
@@ -1,7 +1,16 @@
 package algorithm.test.baekjoon.level08.exam04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // 2903 중앙 이동 알고리즘
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    br.close();
 
+    System.out.println((int)Math.pow(Math.pow(2, n) +1, 2));
   }
 }
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam05/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam05/Main.java
@@ -1,7 +1,26 @@
 package algorithm.test.baekjoon.level08.exam05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // 2292 벌집
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    int cnt = 1;
+    int range = 2;
+
+    if(N==1){
+      System.out.print(1);
+    } else {
+      while (range <= N){
+        range = range + 6*cnt;
+        cnt++;
+      }
+      System.out.print(cnt);
+    }
   }
 }
