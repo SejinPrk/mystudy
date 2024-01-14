da012cde0c387772123278d5f7a444aefec4bc34
SejinPrk <qkrwns9808@naver.com>
2024-01-14 12:28:16 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam06/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam06/Main.java
@@ -1,7 +1,32 @@
 package algorithm.test.baekjoon.level08.exam06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // 1193 분수찾기
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int x = Integer.parseInt(br.readLine());
 
+    int cross = 1;
+    int sum = 0;
+
+    while(true){
+      if(x <= sum + cross){
+        if(cross %2 ==1){
+          System.out.print(cross - (x-sum-1)+ "/"+ (x-sum));
+          break;
+        }
+        else{
+          System.out.print((x-sum) + "/"+ (cross-(x-sum-1)));
+          break;
+        }
+      } else {
+        sum += cross;
+        cross++;
+      }
+    }
   }
 }
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam07/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level08/exam07/Main.java
@@ -1,10 +1,8 @@
 package algorithm.test.baekjoon.level08.exam07;
 
-import java.awt.SystemTray;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.Buffer;
 import java.util.StringTokenizer;
 
 // 2869 달팽이는 올라가고 싶다
@@ -13,7 +11,14 @@ public class Main {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
 
+    int up = Integer.parseInt(st.nextToken());
+    int down = Integer.parseInt(st.nextToken());
+    int length = Integer.parseInt(st.nextToken());
 
+    int day = (length-down) / (up-down);
+    if((length-down) % (up-down) != 0){
+      day++;
+    }
+    System.out.println(day);
   }
-
 }
