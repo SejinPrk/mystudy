0a0ee64a53fdd4fffd1dae40af2157d71e33e9f9
SejinPrk <qkrwns9808@naver.com>
2024-01-16 10:07:13 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level09/exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level09/exam02/Main.java
@@ -1,7 +1,27 @@
 package algorithm.test.baekjoon.level09.exam02;
+
+import java.util.Scanner;
+
 // 2501 약수 구하기
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int K = sc.nextInt();
+    int cnt = 0;
 
+    for (int i = 1; i <= N; i++){
+      if(N % i == 0){
+        cnt++;
+      }
+
+      if (cnt == K){
+        System.out.println(i);
+        break;
+      }
+    }
+    if (cnt < K){
+      System.out.println(0);
+    }
   }
 }
