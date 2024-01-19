3cb3c337208d713dbc0d4a53b6a7d1c57f43d1d9
SejinPrk <qkrwns9808@naver.com>
2024-01-19 17:54:00 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level10/exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level10/exam04/Main.java
@@ -1,7 +1,12 @@
 package algorithm.test.baekjoon.level10.exam04;
 
+import java.util.Scanner;
+
+// 15894 수학은 체육과목 입니다
 public class Main {
   public static void main(String[] args) {
-
+    Scanner sc = new Scanner(System.in);
+    long n = sc.nextLong();
+    System.out.println(4*n);
   }
 }
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level10/exam06/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level10/exam06/Main.java
@@ -1,7 +1,26 @@
 package algorithm.test.baekjoon.level10.exam06;
 
+import java.util.Scanner;
+
+// 10101 삼각형 외우기
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int first = sc.nextInt();
+    int second = sc.nextInt();
+    int last = sc.nextInt();
+    int sum = first + second + last;
 
+    if(first + second + last != 180){
+      System.out.println("Error");
+    } else {
+      if (first == 60 && second == 60 && last == 60) {
+        System.out.println("Equilateral");
+      } else if (first == second || second == last || first == last) {
+        System.out.println("Isosceles");
+      } else {
+        System.out.println("Scalene");
+      }
+    }
   }
 }
