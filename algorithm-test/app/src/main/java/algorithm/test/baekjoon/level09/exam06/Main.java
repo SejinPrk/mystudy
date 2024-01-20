60a75019b02dffe7f4e472232a4cbc2c30bd9bf1
SejinPrk <qkrwns9808@naver.com>
2024-01-20 21:29:18 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level09/exam06/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level09/exam06/Main.java
@@ -1,7 +1,21 @@
 package algorithm.test.baekjoon.level09.exam06;
+
+import java.util.Scanner;
+
 // 11653 소인수분해
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int i = 2; i <= Math.sqrt(n); i++) {
+      while(n % i == 0) {
+        System.out.println(i);
+        n /= i;
+      }
+    }
 
+    if(n != 1) {
+      System.out.println(n);
+    }
   }
 }
