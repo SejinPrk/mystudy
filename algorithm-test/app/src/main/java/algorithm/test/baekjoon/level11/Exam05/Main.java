8d875db45f459e93220aaab38dfe67c721c54d1e
SejinPrk <qkrwns9808@naver.com>
2024-01-24 14:12:44 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level11/Exam05/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level11/Exam05/Main.java
@@ -1,8 +1,19 @@
 package algorithm.test.baekjoon.level11.Exam05;
+
 // 24266 알고리즘 수업-알고리즘의 수행 시간 5
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    long n = Long.parseLong(br.readLine());
+
+    System.out.println(n*n*n);
+    System.out.println(3);
+
+    br.close();
   }
-
 }
