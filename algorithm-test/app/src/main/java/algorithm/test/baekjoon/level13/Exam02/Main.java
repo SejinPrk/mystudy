66838d653059133b5716636c00e79408d7f76a74
SejinPrk <qkrwns9808@naver.com>
2024-01-26 09:52:09 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level13/Exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level13/Exam02/Main.java
@@ -1,8 +1,31 @@
 package algorithm.test.baekjoon.level13.Exam02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // 2231 분해합
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int ans = 0;
 
+
+    for (int i = 1; i <= n; i++){
+      int num = i;
+      int sum = 0;
+
+        while (num != 0){
+          sum += num % 10;
+          num /= 10;
+        }
+
+        if (sum + i == n){
+          ans = i;
+          break;
+        }
+    }
+    System.out.println(ans);
   }
-
 }
