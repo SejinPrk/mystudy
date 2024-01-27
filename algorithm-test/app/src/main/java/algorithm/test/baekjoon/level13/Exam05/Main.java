58173cbc0e422700ff70937e102f0324da8c3a8d
SejinPrk <qkrwns9808@naver.com>
2024-01-27 20:15:38 +0900

    .

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level13/Exam05/Main.java
@@ -0,0 +1,21 @@
+package algorithm.test.baekjoon.level13.Exam05;
+// 1436 영화감독 숌
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    int N = in.nextInt();
+    int num = 666;
+    int cnt = 1;
+
+    while (cnt != N){
+      num++;
+      if(String.valueOf(num).contains("666")) {
+        cnt++;
+      }
+    }
+    System.out.println(num);
+  }
+}
