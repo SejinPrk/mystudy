479f342cb2b94af8d884affca7120e060b2e6de4
SejinPrk <qkrwns9808@naver.com>
2024-01-29 12:42:33 +0900

    .

--- myapp/app-client/doc/ddl.sql
+++ myapp/app-client/doc/ddl.sql
@@ -1,17 +1,39 @@
 -- DDL(Data Definition Language)
 
-create table boards (
- board_no int primary key auto_increment,
- title varchar(255) not null,
- content text not null,
- writer varchar(30) not null,
- created_date datetime null default now()
+create table boards(
+  board_no int primary key auto_increment,
+  title varchar(255) not null,
+  content text not null,
+  writer varchar(30) not null,
+  created_date datetime null default now()
 );
 
-insert into boards(title,content,writer) values('제목1','내용1','홍길동');
-insert into boards(title,content,writer) values('제목2','내용2','임꺽정');
-insert into boards(title,content,writer) values('제목3','내용3','유관순');
-insert into boards(title,content,writer) values('제목4','내용4','안중근');
-insert into boards(title,content,writer) values('제목5','내용5','윤봉길');
+insert into boards(board_no,title,content,writer) 
+values(1, '제목1','내용1','홍길동');
+insert into boards(board_no,title,content,writer) 
+values(2, '제목2','내용2','임꺽정');
+insert into boards(board_no,title,content,writer) 
+alues(3, '제목3','내용3','유관순');
+insert into boards(board_no,title,content,writer) 
+values(4, '제목4','내용4','안중근');
+insert into boards(board_no,title,content,writer) 
+values(5, '제목5','내용5','윤봉길');
 
 select * from boards;
+
+select * from boards where board_no = 3;
+
+update boards set 
+title='okok', 
+content='nono', 
+writer='hoho' 
+where board_no = 3;
+
+delete from boards where board_no=3;
+
+create table assignments(
+  assignment_no int primary key auto_increment,
+  title varchar(255) not null,
+  content text not null,
+  deadline datetime not null()
+);
