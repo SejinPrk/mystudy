4c8fc7c4f886e38674e8ad85ae8ca097b0070ca7
SejinPrk <qkrwns9808@naver.com>
2024-01-29 10:26:08 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level14/Exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level14/Exam04/Main.java
@@ -1,9 +1,29 @@
 package algorithm.test.baekjoon.level14.Exam04;
-//
+// 2751 수 정렬하기 2
 
-public class Main {
-  public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
+    public class Main {
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
 
-  }
-}
+        int n = sc.nextInt();
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++){
+          list.add(sc.nextInt());
+        }
+
+        Collections.sort(list);
+
+        for(int val : list) {
+          sb.append(val).append('\n');
+        }
+        System.out.println(sb);
+      }
+    }
+
