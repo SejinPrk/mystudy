48f0a6ef6a2bb8a6a10cb1c1f966692c9445f8a2
SejinPrk <qkrwns9808@naver.com>
2024-01-30 17:26:01 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level14/Exam06/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level14/Exam06/Main.java
@@ -1,9 +1,21 @@
 package algorithm.test.baekjoon.level14.Exam06;
-//
+// 1427 소트 인사이드
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    char[] arr = br.readLine().toCharArray();
 
+    Arrays.sort(arr);
+
+    for (int i = arr.length-1; i >= 0; i--) {
+      System.out.println(arr[i]);
+    }
   }
 }
