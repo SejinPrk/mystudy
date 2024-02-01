9c6a96a2f4f919d4120702540cab23e25d8c2c4c
SejinPrk <qkrwns9808@naver.com>
2024-02-01 12:12:21 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level14/Exam10/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level14/Exam10/Main.java
@@ -1,5 +1,5 @@
 package algorithm.test.baekjoon.level14.Exam10;
-//
+// 10814 나이순 정렬
 
 public class Main {
   public static void main(String[] args) {
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level14/Exam11/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level14/Exam11/Main.java
@@ -1,5 +1,5 @@
 package algorithm.test.baekjoon.level14.Exam11;
-//
+// 18870 좌표 압축
 
 import java.util.Scanner;
 
