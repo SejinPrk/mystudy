37d933c812276f9fd57084a094903b52e5c792a0
SejinPrk <qkrwns9808@naver.com>
2024-02-04 14:22:57 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level15/Exam01/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level15/Exam01/Main.java
@@ -1,7 +1,33 @@
 package algorithm.test.baekjoon.level15.Exam01;
 
-public class Main {
-  public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    StringBuilder sb = new StringBuilder();
+
+    HashMap<String, Integer> have = new HashMap<>();
+
+    int n = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    for (int i =0; i < n; i++) {
+      have.put(st.nextToken(), 0);
+    }
+    int m = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < m; i++ ){
+      if (have.get(st.nextToken())!= null) {
+        sb.append("1 ");
+      } else {
+        sb.append("0 ");
+      }
+    }
+    System.out.println(sb.toString());
   }
 }
