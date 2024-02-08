e0c3f0ee3731f4d9bf623888bb18161b23fc22e7
SejinPrk <qkrwns9808@naver.com>
2024-02-08 16:14:07 +0900

    .

--- myapp/app-client/src/main/java/bitcamp/myapp/ClientApp.java
+++ myapp/app-client/src/main/java/bitcamp/myapp/ClientApp.java
@@ -11,7 +11,7 @@ public class ClientApp {
 
   public static void main(String[] args) {
     new ClientApp()
-       // .server("192.168.0.49");
+       // .server("192.168.0.49")
         .server("localhost")
         .port(8888)
         .run();
