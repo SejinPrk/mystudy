f411bc005e28fb0c2e283c5657357b1e62770bc0
SejinPrk <qkrwns9808@naver.com>
2024-02-14 12:46:28 +0900

    .

--- myapp/app-server/doc/ddl.sql
+++ myapp/app-server/doc/ddl.sql
@@ -3,7 +3,7 @@
 drop table boards;
 
 create table boards(
-  board_no int primary key auto_increment,
+  board_no int not null,
   title varchar(255) not null,
   content text not null,
   writer varchar(30) not null,
@@ -11,31 +11,9 @@ create table boards(
   created_date datetime null default now()
 );
 
-insert into boards(board_no,title,content,writer,category)
-  values(1, '제목1','내용1','홍길동', 1);
-insert into boards(board_no,title,content,writer,category)
-  values(2, '제목2','내용2','임꺽정', 1);
-insert into boards(board_no,title,content,writer,category)
-  values(3, '제목3','내용3','유관순', 2);
-insert into boards(board_no,title,content,writer,category)
-  values(4, '제목4','내용4','안중근', 2);
-insert into boards(board_no,title,content,writer,category)
-  values(5, '제목5','내용5','윤봉길', 2);
-
-select * 
-from boards;
-
-select * 
-from boards 
-where board_no = 3;
-
-update boards set 
-  title='okok', 
-  content='nono', 
-  writer='hoho' 
-where board_no = 3;
-
-delete from boards where board_no=3;
+alter table borads
+  add constraint primary key (board_no),
+  modify column board_no int not null auto_increment;
 
 
 drop table assignments;
@@ -47,17 +25,6 @@ create table assignments(
   deadline date not null
 );
 
-insert into assignments(assignment_no,title,content,deadline) 
-  values(1,'과제1','내용1','2024-1-1');
-insert into assignments(assignment_no,title,content,deadline) 
-  values(2,'과제2','내용2','2024-2-2');
-insert into assignments(assignment_no,title,content,deadline) 
-  values(3,'과제3','내용3','2024-3-3');
-insert into assignments(assignment_no,title,content,deadline) 
-  values(4,'과제4','내용4','2024-4-4');
-insert into assignments(assignment_no,title,content,deadline) 
-  values(5,'과제5','내용5','2024-5-5');
-
 drop table members;
 
 create table members(
@@ -68,16 +35,7 @@ create table members(
    created_date datetime null default now()
 );
 
-insert into members(email,name,password,created_date)
-  values('user1@test.com','user1',sha2('1111',256),'2024-1-1');
-insert into members(email,name,password,created_date)
-  values('user2@test.com','user2',sha2('1111',256),'2024-2-2');
-insert into members(email,name,password,created_date)
-  values('user3@test.com','user3',sha2('1111',256),'2024-3-3');
-insert into members(email,name,password,created_date)
-  values('user4@test.com','user4',sha2('1111',256),'2024-4-4');
-insert into members(email,name,password,created_date)
-  values('user5@test.com','user5',sha2('1111',256),'2024-5-5');
+
 
 alter table boards
   add column category int not null;
