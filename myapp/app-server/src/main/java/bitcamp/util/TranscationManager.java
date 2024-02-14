d75c05fe3b16a7c8624a5cc16a28e48534a09c9b
SejinPrk <qkrwns9808@naver.com>
2024-02-14 11:47:31 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/util/TranscationManager.java
+++ myapp/app-server/src/main/java/bitcamp/util/TranscationManager.java
@@ -1,17 +1,23 @@
 package bitcamp.util;
 
+import java.sql.Connection;
+
 public class TranscationManager {
+
   ConnectionPool connectionPool;
 
   public TranscationManager(ConnectionPool connectionPool) {
     this.connectionPool = connectionPool;
   }
 
+  public void startTransaction() throws Exception{
+    ConnectionPool.getConnection().setAutoCommit(false);
+  }
   public void commit(){
 
   }
 
   public void rollback(){
-    
+
   }
 }
