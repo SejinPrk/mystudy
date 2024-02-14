b983bed96f8782777826abd84cdef5d7659708bc
SejinPrk <qkrwns9808@naver.com>
2024-02-14 11:59:56 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/handler/board/BoardAddHandler.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/handler/board/BoardAddHandler.java
@@ -5,15 +5,18 @@ import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.vo.Board;
 import bitcamp.util.DBConnectionPool;
 import bitcamp.util.Prompt;
+import bitcamp.util.TransactionManager;
+import com.mysql.cj.x.protobuf.MysqlxExpect.Open.CtxOperation;
+import java.awt.TextComponent;
 import java.sql.Connection;
 
 public class BoardAddHandler extends AbstractMenuHandler {
 
-  DBConnectionPool connectionPool;
+  TransactionManager txManager;
   private BoardDao boardDao;
 
-  public BoardAddHandler(DBConnectionPool connectionPool, BoardDao boardDao) {
-    this.connectionPool = connectionPool;
+  public BoardAddHandler(TransactionManager txManager, BoardDao boardDao) {
+    this.txManager = txManager;
     this.boardDao = boardDao;
   }
 
@@ -26,9 +29,7 @@ public class BoardAddHandler extends AbstractMenuHandler {
 
     Connection con = null;
     try {
-      con = connectionPool.getConnection();
-
-      con.setAutoCommit(false);
+      txManager.startTransaction();
 
       boardDao.add(board);
       boardDao.add(board);
@@ -37,15 +38,13 @@ public class BoardAddHandler extends AbstractMenuHandler {
 
       boardDao.add(board);
 
-      con.commit();
+      txManager.commit();
 
     } catch (Exception e) {
       try {
-        con.rollback();
+        txManager.rollback();
       } catch (Exception e2) {
       }
-    } finally {
-      connectionPool.returnConnection(con);
     }
   }
 }
