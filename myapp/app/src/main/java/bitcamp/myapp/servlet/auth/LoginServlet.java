6ef35a1240819dcd37d450b8fc5d4e84c03d89db
SejinPrk <qkrwns9808@naver.com>
2024-02-16 11:57:24 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/servlet/auth/LoginServlet.java
+++ myapp/app/src/main/java/bitcamp/myapp/servlet/auth/LoginServlet.java
@@ -1,10 +1,18 @@
 package bitcamp.myapp.servlet.auth;
 
-import bitcamp.menu.AbstractMenuHandler;
 import bitcamp.myapp.dao.MemberDao;
+import bitcamp.myapp.dao.mysql.MemberDaoImpl;
 import bitcamp.myapp.vo.Member;
-import bitcamp.util.Prompt;
+import bitcamp.util.DBConnectionPool;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/auth/login")
 public class LoginServlet extends GenericServlet {
@@ -17,11 +25,13 @@ public class LoginServlet extends GenericServlet {
     this.memberDao = new MemberDaoImpl(connectionPool);
   }
 
+  @Override
   public void service(ServletRequest servletRequest, ServletResponse servletResponse)
       throws ServletException, IOException {
+
     // 서블릿 컨테이너가 service()를 호출할 때 넘겨주는 값을
     // HttpServletRequest와 HttpServletResponse이다.
-    // 파라미터로 넘어온 객체를 제대로 사용하고 싶다면 원래 타입으로 형변환하라!
+    // 파라미터로 넘어 온 객체를 제대로 사용하고 싶다면 원래 타입으로 형변환하라!
     HttpServletRequest request = (HttpServletRequest) servletRequest;
     HttpServletResponse response = (HttpServletResponse) servletResponse;
 
@@ -38,37 +48,25 @@ public class LoginServlet extends GenericServlet {
     out.println("  <title>비트캠프 데브옵스 5기</title>");
     out.println("</head>");
     out.println("<body>");
-    out.println("<h1>게시글</h1>");
+    out.println("<h1>과제 관리 시스템</h1>");
     out.println("<h2>로그인</h2>");
 
     try {
       Member member = memberDao.findByEmailAndPassword(email, password);
       if (member != null) {
         request.getSession().setAttribute("loginUser", member);
-        prompt.printf("<p>%s 님 환영합니다.</p>\n", member.getName());
+        out.printf("<p>%s 님 환영합니다.</p>\n", member.getName());
       } else {
-        prompt.println("이메일 또는 암호가 맞지 않습니다.");
+        out.println("<p>이메일 또는 암호가 맞지 않습니다.</p>");
       }
     } catch (Exception e) {
       out.println("<p>로그인 오류!</p>");
       out.println("<pre>");
       e.printStackTrace(out);
       out.println("</pre>");
-  }
-}
-
-  protected void action(Prompt prompt) {
-    String email = prompt.input("이메일? ");
-    String password = prompt.input("암호? ");
-
-    Member member = memberDao.findByEmailAndPassword(email, password);
-    if (member != null) {
-//      prompt.setLoginUser(member);
-//      prompt.setAttribute("loginUser", member);
-      prompt.getSession().setAttribute("loginUser", member);
-      prompt.printf("%s 님 환영합니다.\n", member.getName());
-    } else {
-      prompt.println("이메일 또는 암호가 맞지 않습니다.");
     }
+
+    out.println("</body>");
+    out.println("</html>");
   }
 }
