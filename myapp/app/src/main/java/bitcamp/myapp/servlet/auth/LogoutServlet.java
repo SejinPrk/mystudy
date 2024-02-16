04c984de45ee00d37b74a8a63e6c187c0c9eabc0
SejinPrk <qkrwns9808@naver.com>
2024-02-16 14:56:52 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/servlet/auth/LogoutServlet.java
+++ myapp/app/src/main/java/bitcamp/myapp/servlet/auth/LogoutServlet.java
@@ -1,20 +1,22 @@
 package bitcamp.myapp.servlet.auth;
 
-import bitcamp.menu.AbstractMenuHandler;
-import bitcamp.myapp.servlet.board.HttpServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/auth/logout")
 public class LogoutServlet extends HttpServlet {
 
   public void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
+    request.getSession().invalidate();
+
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
-    request.getSession().invalidate();
 
     out.println("<!DOCTYPE html>");
     out.println("<html lang='en'>");
