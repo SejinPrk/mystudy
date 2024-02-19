586757838ee4e86ff3b499b26752d3b3626db0d4
SejinPrk <qkrwns9808@naver.com>
2024-02-19 13:00:22 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/listener/ContextLoaderListener.java
+++ myapp/app/src/main/java/bitcamp/myapp/listener/ContextLoaderListener.java
@@ -1,5 +1,19 @@
 package bitcamp.myapp.listener;
 
-public class ContextLoaderListener {
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
+public class ContextLoaderListener implements ServletContextListener {
+  // 웹 애플리케이션이 사용할 자원을 준비하고 해제하는 역할
+  @Override
+  public void contextInitialized(ServletContextEvent sce) {
+    System.out.println("웹애플리케이션 자원 준비!");
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent sce) {
+    System.out.println("웹애플리케이션 자원 해제!");
+  }
 }
