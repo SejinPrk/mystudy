13039a545c2c581baf879d72cca0c49c4c3f8f1d
SejinPrk <qkrwns9808@naver.com>
2024-02-20 14:11:15 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/Exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/Exam02/Main.java
@@ -1,7 +1,11 @@
 package algorithm.test.baekjoon.level16.Exam02;
 // 13241 최소공배수
+
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
   }
 }
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/Exam03/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/Exam03/Main.java
@@ -1,7 +1,28 @@
 package algorithm.test.baekjoon.level16.Exam03;
 // 1735 분수 합
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    String str[] = new String[n];
+    int cnt = 0;
+    int sum = 0;
 
+    for(int i = 0; i < n; i++) {
+      str[i] = sc.next();
+      sum += str[i].length();
+    }
+
+    for(int i = 0; i < n; i++) {
+      for (int j = 0; j < str[i].length(); j++) {
+        if(str[n].charAt(j) == str[0].charAt(0)){
+          cnt++;
+        }
+      }
+    }
+
+    System.out.println(sum + " " + cnt);
   }
 }
