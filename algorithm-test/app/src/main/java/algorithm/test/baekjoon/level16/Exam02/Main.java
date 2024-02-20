e5c797794dac4cc3bfccdbb9b0047f0a1e925c99
SejinPrk <qkrwns9808@naver.com>
2024-02-20 17:42:55 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/Exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/Exam02/Main.java
@@ -6,6 +6,20 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
+    long a = sc.nextInt();
+    long b = sc.nextInt();
+
+    long gcd = getGCD(Math.max(a, b), Math.min(a,b));
+
+    System.out.println((a*b)/gcd);
+  }
+
+  public static long getGCD(long a, long b){
+    while(b>0) {
+      long temp = a;
+      a = b;
+      b = temp%b;
+    }
+    return a;
   }
 }
