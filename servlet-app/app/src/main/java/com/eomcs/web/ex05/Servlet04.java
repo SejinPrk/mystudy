6b93b5700da611c99ffa0b6a297945e32d4d8e48
SejinPrk <qkrwns9808@naver.com>
2024-02-23 11:36:02 +0900

    .

--- servlet-app/app/src/main/java/com/eomcs/web/ex05/Servlet04.java
+++ servlet-app/app/src/main/java/com/eomcs/web/ex05/Servlet04.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/ex05/s3")
-public class Servlet04 extends MyHttpServlet2 {
+@WebServlet("/ex05/s4")
+public class Servlet04 extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
