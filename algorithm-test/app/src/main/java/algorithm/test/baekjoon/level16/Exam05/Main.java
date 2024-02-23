5f07552e0013951973cc9ef60d77a0c3dd9370bf
SejinPrk <qkrwns9808@naver.com>
2024-02-23 17:58:44 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/Exam05/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/Exam05/Main.java
@@ -1,7 +1,34 @@
 package algorithm.test.baekjoon.level16.Exam05;
 // 4134 다음 소수
-public class Main {
-  public static void main(String[] args) {
 
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String args[]) throws Exception {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+    for (long i = 0; i < n; i++) {
+      long a = sc.nextLong();
+
+      while (true) {
+        long cnt = 0;
+        for (long j = 2; j <= Math.sqrt(a); j++) {
+          if (a % j == 0) {
+            cnt++;
+            break;
+          }
+        }
+        if (cnt == 0) {
+          if (a == 0 || a == 1) {
+            a = 2;
+          }
+          System.out.println(a);
+          break;
+        }
+        a++;
+      }
+    }
+    sc.close();
   }
 }
