823295753faee956dbb1e268e777e429dd8f9302
SejinPrk <qkrwns9808@naver.com>
2024-02-25 21:07:57 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/Exam08/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/Exam08/Main.java
@@ -1,5 +1,5 @@
 package algorithm.test.baekjoon.level16.Exam08;
-
+// 17103 골드바흐 파티션
 public class Main {
   public static void main(String[] args) {
 
---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level16/Exam09/Main.java
@@ -0,0 +1,8 @@
+package algorithm.test.baekjoon.level16.Exam09;
+// 13909 창문 닫기
+public class Main {
+  public static void main(String[] args) {
+
+  }
+
+}
