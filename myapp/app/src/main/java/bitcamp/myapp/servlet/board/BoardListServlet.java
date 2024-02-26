6baed2192fb2a1a295f2886fd6362495787d6d0d
SejinPrk <qkrwns9808@naver.com>
2024-02-26 11:01:10 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/servlet/board/BoardListServlet.java
+++ myapp/app/src/main/java/bitcamp/myapp/servlet/board/BoardListServlet.java
@@ -69,10 +69,9 @@ public class BoardListServlet extends HttpServlet {
       out.println("</table>");
 
     } catch (Exception e) {
-      out.println("<p>목록 오류!</p>");
-      out.println("<pre>");
-      e.printStackTrace(out);
-      out.println("</pre>");
+      request.setAttribute("message", "목록 오류!");
+      request.setAttribute("exception", e);
+      request.getRequestDispatcher("/error").forward(request, response);
     }
 
     request.getRequestDispatcher("/footer").include(request, response);
