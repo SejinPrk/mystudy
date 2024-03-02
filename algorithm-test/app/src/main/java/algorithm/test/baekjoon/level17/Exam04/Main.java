1202918ea6253c31e5c786c658d005dbe376e08e
SejinPrk <qkrwns9808@naver.com>
2024-03-02 17:02:21 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level17/Exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level17/Exam04/Main.java
@@ -1,8 +1,53 @@
 package algorithm.test.baekjoon.level17.Exam04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 // 4949 균형잡힌 세상
 public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
 
-  public static void main(String[] args) {
+    String s;
 
+    while(true) {
+      s = br.readLine();
+      if (s.equals(".")) {
+        break;
+      }
+      sb.append(solve(s)).append('\n');
+    }
+    System.out.println(sb);
+  }
+
+  public static String solve(String s) {
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+
+      if (c == '(' || c== '[') {
+        stack.push(c);
+      } else if(c == ')') {
+        if (stack.empty() || stack.peek() != '(') {
+          return "no";
+        } else {
+          stack.pop();
+        }
+      } else if (c == ']') {
+        if (stack.empty() || stack.peek() != '[') {
+          return  "no";
+        } else {
+          stack.pop();
+        }
+      }
+    }
+    if (stack.empty()) {
+      return "yes";
+    } else {
+      return "no";
+    }
   }
 }
