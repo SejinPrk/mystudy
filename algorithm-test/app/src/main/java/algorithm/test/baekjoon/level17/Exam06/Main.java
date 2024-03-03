837f8d719045acd62060f093177b70f277287af4
SejinPrk <qkrwns9808@naver.com>
2024-03-03 13:04:13 +0900

    .

---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level17/Exam06/Main.java
@@ -0,0 +1,7 @@
+package algorithm.test.baekjoon.level17.Exam06;
+// 18258 큐 2  
+public class Main {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level17/Exam07/Main.java
@@ -0,0 +1,7 @@
+package algorithm.test.baekjoon.level17.Exam07;
+
+public class Main {
+  public static void main(String[] args) {
+
+  }
+}
