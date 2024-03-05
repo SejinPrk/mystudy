17a0d3fd99dc4ca00b687b2f03156b47e0fc5810
SejinPrk <qkrwns9808@naver.com>
2024-03-05 12:04:53 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/controller/HomeController.java
+++ myapp/app/src/main/java/bitcamp/myapp/controller/HomeController.java
@@ -3,9 +3,9 @@ package bitcamp.myapp.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HomeController implements PageController {
-
-  public String execute(HttpServletRequest request, HttpServletResponse response)
+public class HomeController {
+@RequestMapping
+  public String home(HttpServletRequest request, HttpServletResponse response)
       throws Exception {
     return "/home.jsp";
   }
