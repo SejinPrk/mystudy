f71a7621c3f1ea1ac4bee24520e2c758ce24f0bc
SejinPrk <qkrwns9808@naver.com>
2024-03-05 09:43:15 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level17/Exam08/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level17/Exam08/Main.java
@@ -1,7 +1,34 @@
 package algorithm.test.baekjoon.level17.Exam08;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 // 11866 요세푸스 문제 0
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Queue<Integer> q = new LinkedList<>();
 
+    int N = sc.nextInt();
+    int K = sc.nextInt();
+
+    for (int i = 1; i <= N; i++) {
+      q.add(i);
+    }
+
+    StringBuilder sb = new StringBuilder();
+    sb.append('<');
+
+    while (q.size() > 1) {
+      for (int i = 0; i < K-1; i++) {
+        int val = q.poll();
+        q.offer(val);
+      }
+
+      sb.append(q.poll()).append(", ");
+    }
+    sb.append(q.poll()).append('>');
+    System.out.println(sb);
   }
 }
