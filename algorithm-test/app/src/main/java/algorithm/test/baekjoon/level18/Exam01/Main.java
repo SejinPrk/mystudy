d03e516c43cabb0236ced0ae6035eba94e93fd9a
SejinPrk <qkrwns9808@naver.com>
2024-03-11 15:09:36 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level18/Exam01/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level18/Exam01/Main.java
@@ -1,7 +1,22 @@
 package algorithm.test.baekjoon.level18.Exam01;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 // 15439 베라의 패션
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args)  throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    int N = Integer.parseInt(br.readLine());
+
+    bw.write(N * (N-1) + "\n");
+    bw.flush();
+    br.close();
+    bw.close();
   }
 }
