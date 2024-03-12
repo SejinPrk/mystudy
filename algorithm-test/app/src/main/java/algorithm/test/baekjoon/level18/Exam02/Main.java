767e294b7e8917959f37efc10a45c100c2c6c1f8
SejinPrk <qkrwns9808@naver.com>
2024-03-12 14:52:21 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level18/Exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level18/Exam02/Main.java
@@ -1,8 +1,12 @@
 package algorithm.test.baekjoon.level18.Exam02;
 
+import java.util.Scanner;
+
 // 24723 녹색거탑
 public class Main {
   public static void main(String[] args) {
-
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      System.out.println((int)Math.pow(2, n));
   }
 }
