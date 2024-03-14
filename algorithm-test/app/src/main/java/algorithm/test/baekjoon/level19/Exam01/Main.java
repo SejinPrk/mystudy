0ee3b53268bb7ddbb3e833eacea904445d7efe29
SejinPrk <qkrwns9808@naver.com>
2024-03-14 09:48:50 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level19/Exam01/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level19/Exam01/Main.java
@@ -1,8 +1,20 @@
 package algorithm.test.baekjoon.level19.Exam01;
+
+import java.util.Scanner;
+
 // 1037 약수
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
 
+    while(T-- > 0) {
+      int N = sc.nextInt();
+      max = N > max ? N : max;
+      min = N < min ? N : min;
+    }
+    System.out.println(min * max);
   }
-
 }
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level19/Exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level19/Exam02/Main.java
@@ -1,8 +1,14 @@
 package algorithm.test.baekjoon.level19.Exam02;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // 25192 인사성 밝은 곰곰이
 public class Main {
-  public static void main(String[] args) {
-
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
   }
 
 }
