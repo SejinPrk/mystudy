e8bc0dd52fb287f0af1c2ec35baa6b50d9456887
SejinPrk <qkrwns9808@naver.com>
2024-03-14 16:22:37 +0900

    .

---
+++ mvc-app/app/src/main/java/bitcamp/config/AdminWebApplicationInitializer.java
@@ -0,0 +1,31 @@
+package bitcamp.config;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AdminWebApplicationInitializer extends
+    AbstractAnnotationConfigDispatcherServletInitializer{
+
+  private static Log log = LogFactory.getLog(AdminWebApplicationInitializer.class);
+
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    return new Class[]{RootConfig.class};
+  }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class[]{AdminConfig.class};
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[]{"/admin/*"};
+  }
+  @Override
+  protected String getServletName() {
+    return "admin";
+  }
+
+}
