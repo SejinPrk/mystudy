392bb7e01cc634758f32e0bd464df0e5c59fc199
SejinPrk <qkrwns9808@naver.com>
2024-03-14 14:27:29 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level19/Exam03/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level19/Exam03/Main.java
@@ -1,8 +1,41 @@
 package algorithm.test.baekjoon.level19.Exam03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
 // 26069 붙임성 좋은 총총이
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    StringTokenizer st;
+    String A, B;
+
+    HashSet<String> enter = new HashSet();
+    enter.add("ChongChong");
+
+    int N = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      A = st.nextToken();
+      B = st.nextToken();
+      if (enter.contains(A) || enter.contains(B)) {
+        enter.add(A);
+        enter.add(B);
+      }
+    }
+    br.close();
+
+    bw.write(enter.size() + "\n");
+    bw.flush();
+    bw.close();
   }
 
 }
