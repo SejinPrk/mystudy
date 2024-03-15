0fef863ff61b188d8c00ba543fe7880794aa50f1
SejinPrk <qkrwns9808@naver.com>
2024-03-15 09:51:34 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level19/Exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level19/Exam04/Main.java
@@ -1,8 +1,59 @@
 package algorithm.test.baekjoon.level19.Exam04;
+
+import java.util.Scanner;
+
 // 2108 통계학
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[8001];
 
+    int sum = 0;
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
+    int median = 10000;
+    int mode = 10000;
+
+    for (int i = 0; i < n; i++) {
+      int val = sc.nextInt();
+      sum += val;
+      arr[val + 4000] ++;
+
+      if (max < val) {
+        max = val;
+      }
+      if (min > val) {
+        min = val;
+      }
+    }
+
+    int cnt = 0;
+    int max_mode = 0;
+
+    boolean flag = false;
+
+    for (int i = min + 4000; i <= max + 4000 ; i++) {
+      if (arr[i] > 0) {
+        if (cnt < (n+1)/2) {
+          cnt += arr[i];
+          median = i - 4000;
+        }
+
+        if (max_mode < arr[i]) {
+          max_mode = arr[i];
+          mode = i - 4000;
+          flag = true;
+        } else if (max_mode == arr[i] && flag == true) {
+          mode = i - 4000;
+          flag = false;
+        }
+      }
+    }
+    System.out.println((int)Math.round((double)sum /n));
+    System.out.println(median);
+    System.out.println(mode);
+    System.out.println(max - min);
   }
 
 }
