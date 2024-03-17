1784405a48a7655c575388c7cc72e15cb00492bb
SejinPrk <qkrwns9808@naver.com>
2024-03-17 11:45:32 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level20/Exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level20/Exam02/Main.java
@@ -1,7 +1,19 @@
 package algorithm.test.baekjoon.level20.Exam02;
+
+import java.util.Scanner;
+
 // 10870 피보나치 수 5
 public class Main {
   public static void main(String[] args) {
 
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    System.out.println(fibonacci(N));
+  }
+
+  static int fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n-1) + fibonacci(n-2);
   }
 }
