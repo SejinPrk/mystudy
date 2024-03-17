3c39430eb651bc9180390ab2df0b37c0b4450ddc
SejinPrk <qkrwns9808@naver.com>
2024-03-17 11:43:32 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level20/Exam01/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level20/Exam01/Main.java
@@ -1,7 +1,20 @@
 package algorithm.test.baekjoon.level20.Exam01;
+
+import java.io.*;
+
 // 27433 팩토리얼 2
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine());
+    br.close();
+    System.out.println(factorial(N));
+  }
 
+  static long factorial(int n) {
+    if(n <= 0) {
+      return 1;
+    }
+    return n * factorial(n-1);
   }
 }
