10ae2658372db5cf16cc6a2141255922217feb4e
SejinPrk <qkrwns9808@naver.com>
2024-03-18 12:43:57 +0900

    .

---
+++ mvc-app/app/src/main/java/bitcamp/app1/Controller04_8.java
@@ -0,0 +1,62 @@
+package bitcamp.app1;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.UUID;
+import javax.servlet.ServletContext;
+import javax.servlet.http.Part;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/c04_8")
+public class Controller04_8 {
+
+  // ServletContext는 메서드의 아규먼트로 받을 수 없다.
+  // 의존 객체로 주입 받아야 한다.
+  @Autowired
+  ServletContext sc;
+
+  // 클라이언트가 멀티파트 형식으로 전송한 데이터를 꺼내기
+  // => Servlet API에서 제공하는 Part를 사용하거나
+  //    또는 Spring에서 제공하는 MultipartFile 타입의 아규먼트를 선언하면 된다.
+  //
+  // 주의!
+  // => DispatcherServlet을 web.xml을 통해 배치했다면,
+  //    <multipart-config/> 태그를 추가해야 한다.
+  // => WebApplicationInitializer를 통해 DispatcherServlet을 배치했다면,
+  //    App1WebApplicationInitializer 클래스를 참고하라!
+  //
+
+  // 테스트:
+  // http://.../html/app1/c04_8.html
+  @PostMapping(value = "h1", produces = "text/html;charset=UTF-8")
+  @ResponseBody
+  public String handler1(
+      String name,
+      int age,
+      Part photo // Servlet API의 객체
+  ) throws Exception {
+
+    String filename = null;
+    if (photo != null && photo.getSize() > 0) {
+      filename = UUID.randomUUID().toString();
+      String path = sc.getRealPath("/upload/" + filename);
+      photo.write(path);
+    }
+
+    return "<html><head><title>c04_8/h1</title></head><body>" + "<h1>업로드 결과</h1>" + "<p>이름:" + name
+        + "</p>" + "<p>나이:" + age + "</p>" +
+        // 현재 URL이 다음과 같기 때문에 업로드 이미지의 URL을 이 경로를 기준으로 계산해야 한다.
+        // http://localhost:8080/java-spring-webmvc/app1/c04_8/h1
+        //
+        (filename != null ? "<p><img src='../../upload/" + filename + "'></p>" : "")
+        + "</body></html>";
+  }
+}
