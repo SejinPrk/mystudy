c1be83c9eaef498ef0a611939e2ea13fd49c916c
SejinPrk <qkrwns9808@naver.com>
2024-03-19 16:06:42 +0900

    .

--- mvc-app/app/src/main/java/bitcamp/config/App2Config.java
+++ mvc-app/app/src/main/java/bitcamp/config/App2Config.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.util.UrlPathHelper;
 
 @EnableWebMvc
 @ComponentScan("bitcamp.app2")
@@ -18,5 +20,12 @@ public class App2Config implements WebMvcConfigurer {
         ".jsp"
     );
     return vr;
+  }
+
+  @Override
+  public void configurePathMatch(PathMatchConfigurer configurer) {
+    UrlPathHelper urlPathHelper = new UrlPathHelper();
+    urlPathHelper.setRemoveSemicolonContent(false);
+    configurer.setUrlPathHelper(urlPathHelper);
   }
 }
