c87a787b6e2d2e259717540eafefc8dfb7ab0d4b
SejinPrk <qkrwns9808@naver.com>
2024-03-19 16:01:22 +0900

    .

--- mvc-app/app/src/main/java/bitcamp/app2/Controller02_2.java
+++ mvc-app/app/src/main/java/bitcamp/app2/Controller02_2.java
@@ -2,6 +2,8 @@ package bitcamp.app2;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.MatrixVariable;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,5 +17,29 @@ public class Controller02_2 {
     // Query String 으로 값 받기
     return String.format("name=%s, age=%d", name, age);
   }
+
+  @GetMapping(value = "{value}", produces = "text/plain;charset=UTF-8")
+  @ResponseBody
+  public String handler2(
+      @PathVariable("value") String value,
+      @MatrixVariable(required = false) String name,
+      @MatrixVariable(defaultValue = "20") int age
+  ) {
+    return String.format("value:%s \n name:%s, age:%d", value, name, age);
+  }
+
+  @GetMapping(value = "{team}/{task}", produces = "text/plain;charset=UTF-8")
+  @ResponseBody
+  public String handler3(
+      // 여러 개의 패스 변수가 있을 때 값을 꺼내는 방법
+      @MatrixVariable String name,
+      @MatrixVariable int qty,
+      @MatrixVariable String title,
+      @MatrixVariable int state
+  ) {
+
+    return String.format("team: %s(%d)\n task: %s, %d", name, qty, title, state);
+  }
+
 
 }
--- mvc-app/app/src/main/java/bitcamp/config/App2Config.java
+++ mvc-app/app/src/main/java/bitcamp/config/App2Config.java
@@ -3,10 +3,13 @@ package bitcamp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@EnableWebMvc
 @ComponentScan("bitcamp.app2")
-public class App2Config {
+public class App2Config implements WebMvcConfigurer {
 
   @Bean
   public ViewResolver viewResolver() {
