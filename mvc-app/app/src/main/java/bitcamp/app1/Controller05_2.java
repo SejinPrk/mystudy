691e2c69c4c59ee0ce933564fe8ff38345bea89c
SejinPrk <qkrwns9808@naver.com>
2024-03-19 11:51:38 +0900

    .

--- mvc-app/app/src/main/java/bitcamp/app1/Controller05_2.java
+++ mvc-app/app/src/main/java/bitcamp/app1/Controller05_2.java
@@ -37,5 +37,20 @@ public class Controller05_2 {
     return "/WEB-INF/jsp/c05_2.jsp";
   }
 
+  // 테스트:
+  // http://localhost:9999/eomcs-spring-webmvc/app1/c05_2/h3
+  @GetMapping("h3")
+  public View handler3() {
+    return new JstlView("/WEB-INF/jsp/c05_2.jsp");
+  }
 
+  // 테스트:
+  // http://localhost:9999/eomcs-spring-webmvc/app1/c05_2/h4
+  @GetMapping("h4")
+  public ModelAndView handler4() {
+    System.out.println("===> /app1/c05_2/h4");
+    ModelAndView mv = new ModelAndView();
+    mv.setViewName("/WEB-INF/jsp/c05_2.jsp");
+    return mv;
+  }
 }
