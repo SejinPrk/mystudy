7e9661f7eda6683420c20767c8c4b8175bac4904
SejinPrk <qkrwns9808@naver.com>
2024-03-19 12:39:12 +0900

    .

--- mvc-app/app/src/main/java/bitcamp/app1/Controller05_3.java
+++ mvc-app/app/src/main/java/bitcamp/app1/Controller05_3.java
@@ -3,8 +3,10 @@ package bitcamp.app1;
 import java.util.Map;
 import javax.servlet.ServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/c05_3")
@@ -43,4 +45,40 @@ public class Controller05_3 {
     return "/WEB-INF/jsp/c05_3.jsp";
   }
 
+  // 테스트:
+  //   http://localhost:9999/eomcs-spring-webmvc/app1/c05_3/h3
+  @GetMapping("h3")
+  public String handler3(Model model) {
+
+    // 아규먼트에 Model 타입의 변수를 선언하면
+    // 프론트 컨트롤러는 모델 객체를 만들어 넘겨준다.
+    // 이 객체의 용도는 Map 객체와 같다.
+    //
+    model.addAttribute("name", "홍길동");
+    model.addAttribute("age", 20); // auto-boxing
+    model.addAttribute("working", true); // auto-boxing
+
+    return "/WEB-INF/jsp/c05_3.jsp";
+  }
+
+  // 테스트:
+  //   http://localhost:9999/eomcs-spring-webmvc/app1/c05_3/h4
+  @GetMapping("h4")
+  public ModelAndView handler4() {
+
+    // request handler에서 ModelAndView 객체를 만들어 리턴한다.
+    // => 이 객체의 용도는 Model과 view URL을 함께 리턴하는 것이다.
+    //
+    ModelAndView mv = new ModelAndView();
+
+    // JSP가 사용할 데이터를 담고
+    mv.addObject("name", "홍길동");
+    mv.addObject("age", 20); // auto-boxing
+    mv.addObject("working", true); // auto-boxing
+
+    // JSP 주소도 담는다.
+    mv.setViewName("/WEB-INF/jsp/c05_3.jsp");
+
+    return mv;
+  }
 }
