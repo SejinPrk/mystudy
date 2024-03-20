c33a804c42571ea0a76cb3cfe367ddeafe0b5a3f
SejinPrk <qkrwns9808@naver.com>
2024-03-20 16:16:54 +0900

    .

--- mvc-app/app/src/main/java/bitcamp/app2/Controller06_2.java
+++ mvc-app/app/src/main/java/bitcamp/app2/Controller06_2.java
@@ -12,14 +12,5 @@ public class Controller06_2 {
   @GetMapping("error1")
   public void error1() throws Exception {
     throw new Exception("request handler 오류 발생!");
-    // Request Handler에서 예외를 던졌을 때 처리 절차!
-    // 1) 페이지 컨트롤러 안에 예외 처리기가 있다면,
-    // => 해당 메서드를 호출한다.
-    // 2) @ControllerAdvice 객체에 예외 처리기가 있다면,
-    // => 해당 메서드를 호출한다.
-    // 3) web.xml 에 지정된 오류 처리 기본 페이지가 설정되어 있다면,
-    // => 해당 페이지를 실행한다.
-    // 4) 서블릿 컨테이너의 기본 오류 처리 페이지를 실행한다.
-    //
   }
 }
