0f542c8bd805e566da918306095f6ce68afa0756
SejinPrk <qkrwns9808@naver.com>
2024-03-21 09:44:43 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level20/Exam07/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level20/Exam07/Main.java
@@ -1,7 +1,63 @@
 package algorithm.test.baekjoon.level20.Exam07;
+
+import java.util.Scanner;
+
 // 14888 연산자 끼워넣기
 public class Main {
-  public static void main(String[] args) {
+  public static int MAX = Integer.MIN_VALUE;
+  public static int MIN = Integer.MAX_VALUE;
+  public static int[] operator = new int[4];
+  public static int[] num;
+  public static int N;
 
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    N = sc.nextInt();
+    num = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      num[i] = sc.nextInt();
+    }
+
+    for (int i = 0; i < 4; i++) {
+      operator[i] = sc.nextInt();
+    }
+
+    dfs(num[0], 1);
+
+    System.out.println(MAX);
+    System.out.println(MIN);
+  }
+
+  public static void dfs(int number, int idx) {
+    if (idx == N) {
+      MAX = Math.max(MAX, number);
+      MIN = Math.min(MIN, number);
+      return;
+    }
+    for (int i = 0; i < 4; i++) {
+      if (operator[i] > 0) {
+        operator[i]--;
+
+        switch (i) {
+          case 0:
+            dfs(number + num[idx], idx + 1);
+            break;
+
+          case 1:
+            dfs(number - num[idx], idx + 1);
+            break;
+
+          case 2:
+            dfs(number * num[idx], idx + 1);
+            break;
+
+          case 3:
+            dfs(number / num[idx], idx + 1);
+            break;
+        }
+        operator[i]++;
+      }
+    }
   }
 }
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level20/Exam08/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level20/Exam08/Main.java
@@ -2,8 +2,67 @@ package algorithm.test.baekjoon.level20.Exam08;
 
 // 14889 스타트와 링크
 
-public class Main {
-  public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+  static int N;
+  static int[][] map;
+  static boolean[] visit;
+  static int MIN = Integer.MAX_VALUE;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    N = sc.nextInt();
+
+    map = new int[N][N];
+    visit = new boolean[N];
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        map[i][j] = sc.nextInt();
+      }
+    }
+    comb(0, 0);
+    System.out.println(MIN);
+  }
+
+  static void comb(int idx, int cnt) {
+    if (cnt == N / 2) {
+      diff();
+      return;
+    }
+
+    for (int i = idx; i < N; i++) {
+      if (!visit[i]) {
+        visit[i] = true;
+        comb(i + 1, cnt + 1);
+        visit[i] = false;
+      }
+    }
+  }
+
+  static void diff() {
+    int start = 0;
+    int link = 0;
+
+    for (int i = 0; i < N-1; i++) {
+      for (int j = i+1; j < N; j++) {
+        if (visit[i] == true && visit[j] == true) {
+          start += map[i][j];
+          start += map[j][i];
+        } else if (visit[i] == false && visit[j] == false) {
+          link += map[i][j];
+          link += map[j][i];
+        }
+      }
+    }
+
+    int val = Math.abs(start - link);
+
+    if (val == 0) {
+      System.out.println(val);
+      System.exit(0);
+    }
+    MIN = Math.min(val, MIN);
   }
 }
