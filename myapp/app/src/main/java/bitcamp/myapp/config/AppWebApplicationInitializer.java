0cf45de6536aefb14fa951b634fea8a7c59a2939
SejinPrk <qkrwns9808@naver.com>
2024-03-21 11:42:40 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/config/AppWebApplicationInitializer.java
+++ myapp/app/src/main/java/bitcamp/myapp/config/AppWebApplicationInitializer.java
@@ -3,51 +3,60 @@ package bitcamp.myapp.config;
 import java.io.File;
 import java.util.EnumSet;
 import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
-public class AppWebApplicationInitializer implements WebApplicationInitializer {
+public class AppWebApplicationInitializer extends AbstractDispatcherServletInitializer {
+  ServletContext servletContext;
+  AnnotationConfigWebApplicationContext rootContext;
 
   @Override
-  public void onStartup(ServletContext servletContext) throws ServletException {
-    AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+  protected WebApplicationContext createRootApplicationContext() {
+    rootContext = new AnnotationConfigWebApplicationContext();
     rootContext.register(RootConfig.class);
     rootContext.refresh();
-    rootContext.setServletContext(servletContext);
-    servletContext.addListener(new ContextLoaderListener(rootContext));
+    return rootContext;
+  }
 
+  @Override
+  protected WebApplicationContext createServletApplicationContext() {
     AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
     appContext.register(AppConfig.class);
-    appContext.setParent(rootContext);
-    appContext.setServletContext(servletContext);
+    appContext.setParent(this.rootContext);
+    appContext.setServletContext(this.servletContext);
     appContext.refresh();
-    Dynamic registration = servletContext.addServlet("app", new DispatcherServlet(appContext));
-    registration.addMapping("/app/*");
-    registration.setLoadOnStartup(1);
-    registration.setMultipartConfig(new MultipartConfigElement(
-        new File("./temp").getAbsolutePath(),
-        //new File(System.getProperty("java.io.tmpdir")).getAbsolutePath(),
+    return appContext;
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    return new String[]{"/app"};
+  }
+
+  @Override
+  protected void customizeRegistration(Dynamic registration) {
+    new File("./temp").getAbsolutePath(),
         1024 * 1024 * 10,
         1024 * 1024 * 100,
         1024 * 1024 * 1
     ));
+  }
 
-    CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter("UTF-8");
-    javax.servlet.FilterRegistration.Dynamic filterRegistration =
-        servletContext.addFilter("characterEncodingFilter", characterEncodingFilter);
-    filterRegistration.addMappingForServletNames(
-        EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD, DispatcherType.INCLUDE),
-        false,
-        new String[]{"app"}
-    );
+  @Override
+  protected Filter[] getServletFilters() {
+    return new Filter[] { new CharacterEncodingFilter("UTF-8")};
+  }
 
+  @Override
+  public void onStartup(ServletContext servletContext) throws ServletException {
+    
   }
 }
