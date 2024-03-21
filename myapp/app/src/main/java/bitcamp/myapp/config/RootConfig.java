f39feea1900d75b5e296b999f5d06acd6a732f46
SejinPrk <qkrwns9808@naver.com>
2024-03-21 11:05:43 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/config/RootConfig.java
+++ myapp/app/src/main/java/bitcamp/myapp/config/RootConfig.java
@@ -4,13 +4,17 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
 @ComponentScan(value={
-    "bitcamp.myapp.dao.*",
-    "bitcamp.util.*"
+    "bitcamp.myapp.dao",
+    "bitcamp.util"
 })
 
 @PropertySource({
     "classpath:config/jdbc.properties"
 })
 public class RootConfig {
+
+  public RootConfig() {
+    System.out.println("RootConfig()... 호출됨!");
+  }
 
 }
