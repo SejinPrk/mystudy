33114f5af6e643b200964b3b8105b0edece665b4
SejinPrk <qkrwns9808@naver.com>
2024-03-22 09:48:11 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level21/Exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level21/Exam02/Main.java
@@ -1,7 +1,33 @@
 package algorithm.test.baekjoon.level21.Exam02;
+
+import com.sun.source.tree.BreakTree;
+import java.util.Scanner;
+
 // 9184 신나는 함수 실행
 public class Main {
+  static int[][][] dp = new int[21][21][21];
   public static void main(String[] args) {
 
+    Scanner sc = new Scanner(System.in);
+    while (true) {
+      int a = sc.nextInt();
+      int b = sc.nextInt();
+      int c = sc.nextInt();
+
+      if (a == -1 && b == -1 && c == -1) {
+        break;
+      }
+
+      System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
+    }
   }
+
+  static int w(int x, int y, int z) {
+    if (inRange(x, y, z) && dp[x][y][z] != 0) {
+      return dp[x][y][z];
+    }
+  }
+
+  static boolean inRange(int a, int b, int c) {}
+  return 0 <= a && a <= 20 && 0 <= b &&
 }
