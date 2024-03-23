a18c8c36317b038f20a37add1dda4bcb0563aaa6
SejinPrk <qkrwns9808@naver.com>
2024-03-23 16:18:27 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level21/Exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level21/Exam04/Main.java
@@ -1,7 +1,30 @@
 package algorithm.test.baekjoon.level21.Exam04;
+
+import java.util.Scanner;
+
 // 9461 파도반 수열
 public class Main {
+  public static Long[] sequence = new Long[101];
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
+    sequence[0] = 0L;
+    sequence[1] = 1L;
+    sequence[2] = 1L;
+    sequence[3] = 1L;
+
+    int T = sc.nextInt();
+
+    while(T --> 0) {
+      int N = sc.nextInt();
+      System.out.println(Padovan(N));
+    }
+  }
+
+  public static long Padovan(int N) {
+    if (sequence[N] == null) {
+      sequence[N] = Padovan(N-2) + Padovan(N-3);
+    }
+    return sequence[N];
   }
 }
