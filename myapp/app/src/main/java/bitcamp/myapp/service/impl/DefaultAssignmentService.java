5f5c41ee31d75bb56781c94668656a4092f006d3
SejinPrk <qkrwns9808@naver.com>
2024-03-25 17:16:17 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/service/impl/DefaultAssignmentService.java
+++ myapp/app/src/main/java/bitcamp/myapp/service/impl/DefaultAssignmentService.java
@@ -1,17 +1,21 @@
 package bitcamp.myapp.service.impl;
 
+import bitcamp.myapp.controller.MemberController;
 import bitcamp.myapp.dao.AssignmentDao;
 import bitcamp.myapp.service.AssignmentService;
 import bitcamp.myapp.vo.Assignment;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class DefaultAssignmentService implements AssignmentService {
 
+  private static final Log log = LogFactory.getLog(DefaultAssignmentService.class);
   private final AssignmentDao assignmentDao;
 
   @Override
