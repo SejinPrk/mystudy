15faf8bbbd9f85a1b85dca328b74c0b91caa2862
SejinPrk <qkrwns9808@naver.com>
2024-03-26 12:11:06 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/config/AppConfig.java
+++ myapp/app/src/main/java/bitcamp/myapp/config/AppConfig.java
@@ -2,12 +2,17 @@ package bitcamp.myapp.config;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @ComponentScan({"bitcamp.myapp.controller"})
 public class AppConfig {
@@ -25,6 +30,26 @@ public class AppConfig {
 
   @Bean
   public ViewResolver viewResolver() {
+
     return new InternalResourceViewResolver("/WEB-INF/jsp/", ".jsp");
+  }
+
+  @Bean
+  public SpringResourceTemplateResolver templateResolver(ApplicationContext applicationContext){
+    SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+    templateResolver.setApplicationContext(applicationContext);
+    templateResolver.setPrefix("/WEB-INF/templates/");
+    templateResolver.setSuffix(".html");
+    templateResolver.setTemplateMode(TemplateMode.HTML);
+    templateResolver.setCacheable(true);
+    return templateResolver;
+  }
+
+  @Bean
+  public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver){
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver);
+    templateEngine.setEnableSpringELCompiler(true);
+    return templateEngine;
   }
 }
