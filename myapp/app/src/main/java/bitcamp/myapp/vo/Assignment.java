e66b9caf491f9fecbe3bd2927f698d4e738b280e
SejinPrk <qkrwns9808@naver.com>
2024-03-26 11:35:01 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/vo/Assignment.java
+++ myapp/app/src/main/java/bitcamp/myapp/vo/Assignment.java
@@ -3,12 +3,6 @@ package bitcamp.myapp.vo;
 import java.io.Serializable;
 import java.sql.Date;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @Data // = @Getter + @Setter + @ToString + @EqualsAndHashCode + @RequiredArgsConstructor
 public class Assignment implements Serializable {
--- myapp/app/src/main/java/bitcamp/myapp/vo/AttachedFile.java
+++ myapp/app/src/main/java/bitcamp/myapp/vo/AttachedFile.java
@@ -5,8 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-// Mybatis가 사용할 기본 생성자를 만들어야 한다.
-@NoArgsConstructor
+@NoArgsConstructor // Mybatis 가 사용할 기본 생성자를 만들어야 한다.
 @AllArgsConstructor
 @Builder
 @Data
