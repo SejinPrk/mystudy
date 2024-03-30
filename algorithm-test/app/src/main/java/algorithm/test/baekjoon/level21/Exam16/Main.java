56bfa0833a1e437faba8d9eb1148672c8fa5f535
SejinPrk <qkrwns9808@naver.com>
2024-03-30 17:41:27 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level21/Exam16/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level21/Exam16/Main.java
@@ -1,7 +1,47 @@
 package algorithm.test.baekjoon.level21.Exam16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 // 12865 평범한 배낭
 public class Main {
-  public static void main(String[] args) {
+  static Integer[][] dp;
+  static int[] W;
+  static int[] V;
+   public static void main(String[] args) throws IOException {
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
+     int N = Integer.parseInt(st.nextToken());
+     int K = Integer.parseInt(st.nextToken());
+
+     W = new int[N];
+     V = new int[N];
+
+     dp = new Integer[N][K+1];
+
+     for (int i = 0; i < N; i++) {
+       st = new StringTokenizer(br.readLine(), " ");
+       W[i] = Integer.parseInt(st.nextToken());
+       V[i] = Integer.parseInt(st.nextToken());
+     }
+     System.out.println(knapsack(N-1, K));
+  }
+
+  static int knapsack(int i, int k) {
+    if (i < 0)
+      return 0;
+
+      if (dp[i][k] == null) {
+
+        if (W[i] > k) {
+          dp[i][k] = knapsack(i - 1, k);
+        } else {
+          dp[i][k] = Math.max(knapsack(i - 1, k), knapsack(i - 1, k - W[i]) + V[i]);
+        }
+      }
+      return dp[i][k];
   }
 }
