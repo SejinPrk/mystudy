7fbd21aca64d9c24cb8dfcd3cd0387f1fd629323
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-04-04 14:19:06 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level23/Exam01/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level23/Exam01/Main.java
@@ -1,8 +1,29 @@
 package algorithm.test.baekjoon.level23.Exam01;
+
+import java.util.Scanner;
+
 // 11047 동전 0
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
+    int N = sc.nextInt();
+    int K = sc.nextInt();
+    int[] coin = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      coin[i] = sc.nextInt();
+    }
+    int cnt = 0;
+
+    for (int i = N-1; i >= 0; i--) {
+      if (coin[i] <= K)
+      {
+      cnt+= (K / coin[i]);
+      K = K % coin[i];
+      }
+    }
+    System.out.println(cnt);
   }
 
 }
