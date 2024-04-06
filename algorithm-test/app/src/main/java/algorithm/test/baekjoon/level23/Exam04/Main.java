053cf7987cf6dc7b94bdc954dff1de7033a57e63
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-04-06 22:52:04 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level23/Exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level23/Exam04/Main.java
@@ -1,8 +1,29 @@
 package algorithm.test.baekjoon.level23.Exam04;
+
+import java.util.Scanner;
+
 // 1541 잃어버린 괄호
 public class Main {
   public static void main(String[] args) {
 
-  }
+    Scanner sc = new Scanner(System.in);
+    int sum = Integer.MAX_VALUE;
+    String [] sub = sc.nextLine().split("-");
 
+    for (int i = 0; i < sub.length; i++) {
+      int tmp = 0;
+      String[] add = sub[i].split("\\+");
+
+      for (int j = 0; j < add.length; j++) {
+        tmp += Integer.parseInt(add[j]);
+      }
+
+      if (sum == Integer.MAX_VALUE) {
+        sum = tmp;
+      } else {
+        sum -= tmp;
+      }
+    }
+    System.out.println(sum);
+  }
 }
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level23/Exam05/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level23/Exam05/Main.java
@@ -1,8 +1,10 @@
 package algorithm.test.baekjoon.level23.Exam05;
+
+import java.util.Scanner;
+
 // 13305 주유소
 public class Main {
   public static void main(String[] args) {
-
   }
 
 }
