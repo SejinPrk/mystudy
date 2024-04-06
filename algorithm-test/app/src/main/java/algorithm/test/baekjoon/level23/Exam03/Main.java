d7cd1e5d70749b70b8ad61e20cd79c30ff1250c2
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-04-06 22:47:20 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level23/Exam03/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level23/Exam03/Main.java
@@ -1,8 +1,28 @@
 package algorithm.test.baekjoon.level23.Exam03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 // 11399 ATM
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int[] arr = new int[N];
 
+    for (int i = 0; i < N; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    Arrays.sort(arr);
+
+    int prev = 0;
+    int sum = 0;
+
+    for (int i = 0; i < N; i++) {
+      sum += prev + arr[i];
+      prev += arr[i];
+    }
+    System.out.println(sum);
   }
-
 }
