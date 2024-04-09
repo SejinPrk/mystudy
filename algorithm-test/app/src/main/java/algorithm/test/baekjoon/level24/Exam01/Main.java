9c857dc33f073fb7b1130c5f35fca4680ddd8e6e
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-04-09 09:47:38 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level24/Exam01/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level24/Exam01/Main.java
@@ -1,3 +1,55 @@
 package algorithm.test.baekjoon.level24.Exam01;
 
-c
+import java.util.Scanner;
+
+// 2630 색종이 만들기
+public class Main {
+  public static int white = 0;
+  public static int blue = 0;
+  public static int[][] board;
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    board = new int[N][N];
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        board[i][j] = sc.nextInt();
+      }
+    }
+    partition(0, 0, N);
+
+    System.out.println(white);
+    System.out.println(blue);
+  }
+
+  public static void partition(int row, int col, int size) {
+    if (color(row, col, size)) {
+      if (board[row][col] == 0) {
+        white++;
+      } else {
+        blue++;
+      }
+      return;
+    }
+    int newSize = size/2;
+
+    partition(row, col, newSize);
+    partition(row, col + newSize, newSize);
+    partition(row + newSize, col, newSize);
+    partition(row + newSize, col + newSize, newSize);
+  }
+
+  public static boolean color(int row, int col, int size) {
+    int color = board[row][col];
+
+    for (int i = row; i < row + size; i++) {
+      for (int j = col; j < col + size; j++) {
+        if (board[i][j] != color) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level24/Exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level24/Exam02/Main.java
@@ -1,5 +1,55 @@
 package algorithm.test.baekjoon.level24.Exam02;
+
+import java.util.Scanner;
+
 // 1992 쿼드트리
 public class Main {
+  public static int[][] img;
+  public static StringBuilder sb= new StringBuilder();
+  public static void main(String[] args) {
 
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    img = new int[N][N];
+
+    for (int i = 0; i < N; i++) {
+      String str = sc.next();
+
+      for (int j = 0; j < N; j++) {
+        img[i][j] = str.charAt(j) - '0';
+      }
+    }
+    QuadTree(0,0,N);
+    System.out.println(sb);
+  }
+
+  public static void QuadTree(int a, int b, int size) {
+    if (isPossible(a, b, size)) {
+      sb.append(img[a][b]);
+      return;
+    }
+
+    int newSize = size / 2;
+    sb.append('(');
+
+    QuadTree(a, b, newSize);
+    QuadTree(a, b + newSize, newSize);
+    QuadTree(a + newSize, b, newSize);
+    QuadTree(a + newSize, b + newSize, newSize);
+
+    sb.append(')');
+  }
+
+  public static boolean isPossible(int a, int b, int size) {
+    int value = img[a][b];
+
+    for (int i = a; i < a+ size; i++) {
+      for (int j = b; j < b + size; j++) {
+        if (value != img[i][j]) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 }
