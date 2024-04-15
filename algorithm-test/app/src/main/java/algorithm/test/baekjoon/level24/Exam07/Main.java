502a459d7bd4f8041206b4751de8f2d441efe28a
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-04-15 09:53:11 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level24/Exam07/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level24/Exam07/Main.java
@@ -1,5 +1,18 @@
 package algorithm.test.baekjoon.level24.Exam07;
+
+import java.util.Scanner;
+
 // 10830 행렬 제곱
 public class Main {
+ final static int mod = 1000;
+ public static int n;
+ public static int [][] origin;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+    long B = sc.nextLong();
+
+  }
 
 }
