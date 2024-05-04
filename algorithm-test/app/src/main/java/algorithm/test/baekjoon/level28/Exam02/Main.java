15ba77f5add146d912433dff6b542916367b3c34
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-05-04 15:14:08 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level28/Exam02/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level28/Exam02/Main.java
@@ -1,5 +1,35 @@
 package algorithm.test.baekjoon.level28.Exam02;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
 
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    Stack<Integer> stack = new Stack<>();
+    int N = sc.nextInt();
+    int[] arr = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      arr[i] = sc.nextInt();
+    }
+
+    for (int i = 0; i < N; i++) {
+      while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+        arr[stack.pop()] = arr[i];
+      }
+      stack.push(i);
+    }
+
+    while (!stack.isEmpty()) {
+      arr[stack.pop()] = -1;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < N; i++) {
+      sb.append(arr[i]).append(' ');
+    }
+    System.out.println(sb);
+  }
 }
