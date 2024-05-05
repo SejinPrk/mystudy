af34433ce6c7a0e1023e613507df4698db6f1a4f
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-05-06 00:04:00 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level28/Exam05/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level28/Exam05/Main.java
@@ -22,6 +22,28 @@ public class Main {
     for (int i = 0; i < N; i++) {
       int n = Integer.parseInt(br.readLine());
       Pair pair = new Pair(n, 1);
+
+      while (!stack.empty() && stack.peek().height <= n) {
+        Pair pop = stack.pop();
+        cnt += pop.cnt;
+        if (pop.height == n)
+          pair.cnt += pop.cnt;
+      }
+
+      if (!stack.empty())
+        cnt++;
+      stack.push(pair);
+    }
+        bw.write(cnt + "\n");
+        bw.flush();
+  }
+
+  static class Pair {
+    int height;
+    int cnt;
+    Pair(int height, int cnt) {
+      this.height = height;
+      this.cnt = cnt;
     }
   }
 }
