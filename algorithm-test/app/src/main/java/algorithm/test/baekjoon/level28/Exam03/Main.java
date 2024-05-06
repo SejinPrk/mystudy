34569f154e580a3b6cc3f917205f40c271d9fa4a
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-05-06 11:14:45 +0900

    .

--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level28/Exam03/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level28/Exam03/Main.java
@@ -1,5 +1,44 @@
 package algorithm.test.baekjoon.level28.Exam03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 // 17299 오등큰수
 public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuffer sb = new StringBuffer();
 
+    int N = Integer.parseInt(br.readLine());
+    int[] data = new int[N];
+    int[] cnt = new int[1000001];
+    int[] ans = new int[N];
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      data[i] = Integer.parseInt(st.nextToken());
+      cnt[data[i]]++;
+    }
+
+    Stack<Integer> stack = new Stack<>();
+    for (int i = 0; i < N; i++) {
+      while(!stack.isEmpty() && cnt[data[stack.peek()]] < cnt[data[i]]) {
+        ans[stack.pop()] = data[i];
+      }
+      stack.add(i);
+    }
+
+    while (!stack.isEmpty()) {
+      ans[stack.pop()] = -1;
+    }
+
+    for (int i = 0; i < N; i++) {
+      sb.append(ans[i]).append(" ");
+    }
+
+    System.out.println(sb);
+  }
 }
--- algorithm-test/app/src/main/java/algorithm/test/baekjoon/level28/Exam04/Main.java
+++ algorithm-test/app/src/main/java/algorithm/test/baekjoon/level28/Exam04/Main.java
@@ -1,5 +1,13 @@
 package algorithm.test.baekjoon.level28.Exam04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 // 1725 히스토그램
 public class Main {
+
 
 }
